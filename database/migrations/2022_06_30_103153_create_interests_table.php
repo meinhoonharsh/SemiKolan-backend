cbc9d80758ccc4d96e8834939add928252a5f036
meinhoonharsh <harshprogrammer782@gmail.com>
2022-06-30 16:16:19 +0530

    Added Fields in Interest Table

--- database/migrations/2022_06_30_103153_create_interests_table.php
+++ database/migrations/2022_06_30_103153_create_interests_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('interests', function (Blueprint $table) {
+            // name, image, icon, description, active
             $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
+        
     }
 
     /**

eee2e28f1805ab468c5ebb91b0d3d219347feaa7
meinhoonharsh <harshprogrammer782@gmail.com>
2022-06-30 17:08:11 +0530

    Fixed Model Import Issue

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Post;
+use App\Models\Posts;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -9,7 +9,7 @@ class PostController extends Controller
     //
     public function getAllPosts()
     {
-        $posts = Post::where('active', true)->get();
+        $posts = Posts::where('active', true)->get();
         return $this->sendResponse($posts, 'Posts retrieved successfully.');
     }
 }

da76abb4eb66051ffebfa8b4b2e06d3e14b82da0
meinhoonharsh <harshprogrammer782@gmail.com>
2022-06-30 16:20:37 +0530

    Added fields in posts table

--- database/migrations/2022_06_30_103333_create_posts_table.php
+++ database/migrations/2022_06_30_103333_create_posts_table.php
@@ -14,7 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
+            // posted_by, title, caption, image, interests<array>, active
             $table->id();
+            $table->foreignId('posted_by');
+            $table->string('title')->nullable();
+            $table->string('caption')->nullable();
+            $table->string('image')->nullable();
+            $table->json('interests')->default([]);
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
